0cf5559e1ec6944f0596b69cb33dbf5e5fbee60f
Jeffrey Lin <jlin329@gatech.edu>
2017-01-26 15:28:35 -0500

    Edited Person3.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -4,8 +4,8 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  reversed string 
  *  
- *  @author  Bob
- *  @version 1.1
+ *  @author Jeffrey Lin
+ *  @version 2.0
  */
 public class Person3 {
    /** Holds the persons real name */  
@@ -42,7 +42,10 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+        String str = "";
+        for (int i = 0; i < input.length; i++) {
+            str = input.charAt(i) + str;
+        }
+        return str;
 	}
 }
